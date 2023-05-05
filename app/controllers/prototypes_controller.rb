b8d1f2efb2e5bf74fefdbe3642476ae6c5d02980
tomoyaohashi <t.ohashi1016@gmail.com>
2023-05-05 14:59:16 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
   end
 end
